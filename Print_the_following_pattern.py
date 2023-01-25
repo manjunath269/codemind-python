1c508f52433a3a3147f8a7666889562f9284a0eb
manjunath269 <119472017+manjunath269@users.noreply.github.com>
2023-01-25 09:33:52 +0530

    row = int(input()) for i in range(1,row+1):     for j in range(1, row+1-i):         print(' ', end='')     for j in range(1,i+1):         print(chr(64+j), end='')     for j in range(i-1,0,-1):      print(chr(64+j), end='')     print()

---
+++ Print_the_following_pattern.py
@@ -0,0 +1,9 @@
+row = int(input())
+for i in range(1,row+1):
+    for j in range(1, row+1-i):
+        print(' ', end='')
+    for j in range(1,i+1):
+        print(chr(64+j), end='')
+    for j in range(i-1,0,-1):
+     print(chr(64+j), end='')
+    print()
